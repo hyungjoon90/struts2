ddacc3e0ae951d8870df1a902f25d9d203f489b3
hyungjoon90 <hyungjoon112@gmail.com>
2018-06-15 15:43:49 +0900

    struts maven project setting

---
+++ src/main/java/com/bit/struts2/action/DefaultAction.java
@@ -0,0 +1,13 @@
+package com.bit.struts2.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class DefaultAction implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return this.SUCCESS;
+	}
+
+}
